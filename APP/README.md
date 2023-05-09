8c58cbcb731389c3d6cfbadb08e367a5db7de38d
Daaanxx <dannxko@gmail.com>
2023-05-09 17:59:27 +0100

    Finishing work

---
+++ APP/README.md
@@ -0,0 +1,44 @@
+
+  ## Project Tittle
+
+   README Generator 2.0
+
+  ## Table of Contents 
+
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [How to Contribute](#how-to-contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+## Description
+
+This project is a tool to ...
+
+## Installation
+
+To install this, please do the following ...
+
+## Usage
+
+To use this tool, go ahead and ...
+
+## License
+
+MIT License
+
+## How to Contribute
+
+If you would like to contribute, then ...
+
+## Tests
+
+To test the App use ...
+
+## Questions
+
+In case of any qestion please reach out to my by using my email: Git@Mail, or contact me on GitHub GItUSER.
+  
--- APP/index.js
+++ APP/index.js
@@ -1,4 +1,10 @@
-// LESSON CODE
+// TO DO
+//
+// Stoped at TEST section
+// Add multiple option choices for licensec
+//
+//
+// CODE!
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
@@ -38,9 +44,14 @@ const uPrompt = () =>
       message: "Let users know how to test/run your application:  ",
     },
     {
-      type: "input",
+      type: "list",
       name: "license",
       message: "Choose your license: ",
+      choices: [
+        "Mozilla Public License 2.0",
+        "Apache License 2.0",
+        "MIT License",
+      ],
     },
     {
       type: "input",
@@ -56,20 +67,24 @@ const uPrompt = () =>
 
 const generateREADME = (uAnswers) =>
   `
-  # ${uAnswers.tittle}
+  ## Project Tittle
+
+   ${uAnswers.tittle}
+
+  ## Table of Contents 
+
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [How to Contribute](#how-to-contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
 ## Description
 
 ${uAnswers.description}
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
 
 ## Installation
 
@@ -79,31 +94,9 @@ ${uAnswers.install}
 
 ${uAnswers.usage}
 
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
+${uAnswers.license}
 
 ## How to Contribute
 
@@ -112,6 +105,10 @@ ${uAnswers.contributing}
 ## Tests
 
 ${uAnswers.test}
+
+## Questions
+
+In case of any qestion please reach out to my by using my email: ${uAnswers.email}, or contact me on GitHub ${uAnswers.gitUsername}.
   `;
 
 uPrompt()
